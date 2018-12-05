249b7ab9a67659ac629c35d62d1524f5068570f5
Rock <CASEY_ROCK@HMS.HARVARD.EDU>
2018-12-05 16:06:15 -0500

    first commit

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : Character {
+	private bool charInRange;
+	public string script;
+
+	// Use this for initialization
+	void Start () {
+		charInRange = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (charInRange && Input.GetKeyDown(KeyCode.Space))
+		{
+			Speak();
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.gameObject.tag == "Player")
+		{
+			charInRange = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			charInRange = false;
+		}
+	}
+
+	private void Speak()
+	{
+		Debug.Log(script);
+	}
+}
